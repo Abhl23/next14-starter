8bb82a21b7302ddce29b4ee6a7693b4d3a77bff7
abhishek-immanent <abhishekbhatt.immanent@gmail.com>
2024-02-21 11:32:19 +0530

    make single blog post design

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,5 +1,47 @@
-const SingleBlogPage = () => {
-  return <div>SingleBlogPage</div>;
+import Image from "next/image";
+import styles from "./singlePost.module.css";
+
+const SinglePostPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/19781192/pexels-photo-19781192/free-photo-of-you-called-for-a-ride.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt="blog-image"
+          fill
+          className={styles.img}
+        />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image
+            src="https://images.pexels.com/photos/19781192/pexels-photo-19781192/free-photo-of-you-called-for-a-ride.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt="avatar-image"
+            className={styles.avatar}
+            height={50}
+            width={50}
+          />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>Terry Jefferson</span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>20.02.2024</span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged.
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default SingleBlogPage;
+export default SinglePostPage;
--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 import styles from "./links.module.css";
 import NavLink from "./navLink/navLink";
+import Image from "next/image";
 
 const links = [
   {
@@ -46,12 +47,14 @@ const Links = () => {
           <NavLink item={{ title: "Login", path: "/login" }} />
         )}
       </div>
-      <button
+      <Image
+        src="/menu.png"
+        alt=""
+        width={30}
+        height={30}
         onClick={() => setOpen((prevState) => !prevState)}
         className={styles.menuButton}
-      >
-        Menu
-      </button>
+      />
       {open && (
         <div className={styles.mobileLinks}>
           {links.map((link) => (
